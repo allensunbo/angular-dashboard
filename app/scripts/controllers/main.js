29b117e9b39a428f58312add175ee8e5220f3170
allensunbo <allensunbo@gmail.com>
2015-03-06 20:09:35 -0500

    add highcharts

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -25,37 +25,12 @@ app.controller('MyCtrl', function ($scope, TestData) {
 
   }
 
-  $scope.addPortfolio = function () {
-    var widget1 = new DashboardWidget('Summary', 'SUMMARY', {});
-    var widget2 = new DashboardWidget('Risk', 'RISK', {});
-    var widget3 = new DashboardWidget('Return', 'RETURN', {});
-
-    var widgetRow1 = new DashboardWidgetRow();
-    var widgetRow2 = new DashboardWidgetRow();
-    var widgetRow3 = new DashboardWidgetRow();
-    widgetRow1.addWidget(widget1);
-    widgetRow1.addWidget(widget1);
-    widgetRow2.addWidget(widget2);
-    widgetRow2.addWidget(widget2);
-    widgetRow3.addWidget(widget3);
-    widgetRow3.addWidget(widget3);
-
-    var page1 = new DashboardEditorPage('Page 1');
-    page1.addWidgetRow(widgetRow1);
-    page1.addWidgetRow(widgetRow2);
-
-    var page2 = new DashboardEditorPage('page 2');
-    page2.addWidgetRow(widgetRow2);
-    page2.addWidgetRow(widgetRow3);
-
-    var editor = new DashboardEditor('First Dashboard');
-    editor.addPage(page1);
-    editor.addPage(page2);
-
+  $scope.showDemo = function () {
+    var editor = getSampleEditor();
     var editors = new DashboardEditors();
     editors.addEditor(editor);
 
-    var editorCopy = angular.copy(editor);
+    var editorCopy = getSampleEditor();
     editorCopy.name = 'Another Dashboard!';
     editorCopy.pages[0].name = 'Portfolio Summary';
     editorCopy.pages[1].name = 'Risk Decomposition';
@@ -65,13 +40,100 @@ app.controller('MyCtrl', function ($scope, TestData) {
     console.log(editors);
     $scope.editors = editors;
 
-  }
+  };
+
+  $scope.addPortfolio = function () {
+
+  };
 
   $scope.removeRow = function (row) {
 
   }
 
+  $scope.chartConfig = {
+    options: {
+      chart: {
+        type: 'bar'
+      }
+    },
+    series: [{
+      data: [10, 15, 12, 8, 7]
+    }],
+    title: {
+      text: 'Hello'
+    }
+  }
+
 });
+
+function getSampleEditor() {
+  var widget1 = new DashboardWidget('Summary', 'SUMMARY', {});
+  var widget2 = getRiskWidget();
+  var widget3 = new DashboardWidget('Return', 'RETURN', {});
+
+  var widgetRow1 = new DashboardWidgetRow();
+  var widgetRow2 = new DashboardWidgetRow();
+  var widgetRow3 = new DashboardWidgetRow();
+  //widgetRow1.addWidget(widget1);
+  //widgetRow1.addWidget(widget1);
+  widgetRow2.addWidget(widget2);
+  widgetRow2.addWidget(widget2);
+  widgetRow3.addWidget(widget3);
+  widgetRow3.addWidget(widget3);
+
+  var page1 = new DashboardEditorPage('Page 1');
+  page1.addWidgetRow(widgetRow1);
+  page1.addWidgetRow(widgetRow2);
+
+  var page2 = new DashboardEditorPage('page 2');
+  page2.addWidgetRow(widgetRow2);
+  page2.addWidgetRow(widgetRow3);
+
+  var editor = new DashboardEditor('First Dashboard');
+  editor.addPage(page1);
+  editor.addPage(page2);
+
+  return editor;
+}
+
+function getRiskWidget() {
+  var riskWidget = new DashboardWidget('Risk', 'RISK', {
+    options: {
+      chart: {
+        type: 'bar'
+      }
+    },
+    plotOptions: {
+      bar: {
+        size: '100%'
+      }
+    },
+    series: [{
+      data: randomData()
+    }],
+    title: {
+      text: 'Risk'
+    },
+    loading: false,
+    xAxis: {
+      labels: {
+        formatter: function () {
+          return ['Size', 'Momentum', 'Growth', 'Country', 'Currency'][this.value];
+        }
+      }
+    }
+
+  });
+  return riskWidget;
+}
+
+function randomData() {
+  var data = [];
+  for (var i = 0; i < 5; i++) {
+    data.push(Math.ceil(Math.random() * 20));
+  }
+  return data;
+}
 
 app.factory('TestData', function () {
   function randomData() {
--- app/scripts/models/dashboardEditor.js
+++ app/scripts/models/dashboardEditor.js
@@ -1,5 +1,7 @@
-function DashboardEditor(name) {
+function DashboardEditor(name, type) {
+  this.id = uuid();
   this.name = name;
+  this.type = type;
   this.pages = [];
 }
 DashboardEditor.prototype.addPage = function (page) {
@@ -12,4 +14,11 @@ DashboardEditor.prototype.removePage = function (page) {
 DashboardEditor.prototype.serialize = function () {
 
 };
+
+function uuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
 
